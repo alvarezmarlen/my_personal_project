316699f8124cd93b7f90673c4c0ea307df9b0faf
alvarezmarlen <marlenjosefalvarez@gmail.com>
2026-05-13 00:05:43 +0200

    responsive del nav

--- assets/js/modules/ui.js
+++ assets/js/modules/ui.js
@@ -1,8 +1,8 @@
-
+//CODIGO QUE RENDERIZA AL NAV Y EL FOOTER
 export async function cargarComponentes(idContenedor, rutaArchivo) {
     try {
         const respuesta = await fetch(rutaArchivo);
-        if (respuesta.ok) {
+        if (respuesta.ok) {444
             // Aquí no usamos .json(), sino .text() porque es código HTML
             const html = await respuesta.text();
             document.getElementById(idContenedor).innerHTML = html;
